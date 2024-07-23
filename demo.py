43bd269952a72670628dfe7d876ab2c968b133b7
vijay1 <vijaykumarynh7@gmail.com>
2024-07-23 11:01:19 +0530

    switch

--- demo.py
+++ demo.py
@@ -1,26 +1,22 @@
 import random
-import math
 
-wage_per_hour = 20
-full_day = 8
-half_day = 4
+def main():
+    wage_per_hour = 20
+    full_day_hours = 8
+    half_day_hours = 4
+    print("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======\n")
 
-print("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======")
-print("")
+    emp_check = random.randint(0, 2)  # Generates 0, 1, or 2
 
-emp_check = math.floor(random.random() * 10) % 3
-if emp_check == 0:
-    print("Employee is Present")
-    print("")
-    print(f"The daily wage of Employee is : {wage_per_hour * full_day}")
-    print("")
-elif emp_check == 1:
-    print("Employee is Present but working part time")
-    print("")
-    print(f"The daily wage of Employee is : {wage_per_hour * half_day}")
-    print("")
-else:
-    print("Employee is Absent")
-    print("")
-    print("The daily wage of Employee is : 0")
-    print("")
+    if emp_check == 0:
+        print("Employee is Present")
+        print(f"The daily wage of Employee is: {wage_per_hour * full_day_hours}\n")
+    elif emp_check == 1:
+        print("Employee is Present but working part time")
+        print(f"The daily wage of Employee is: {wage_per_hour * half_day_hours}\n")
+    elif emp_check == 2:
+        print("Employee is Absent")
+        print("The daily wage of Employee is: 0\n")
+
+if __name__ == "__main__":
+    main()
